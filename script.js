21ca48e0923d866a18cdc33821880ad48a4dfdc7
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2024-05-22 18:52:30 +0530

    DOM Adding

---
+++ script.js
@@ -0,0 +1,8 @@
+let cont = document.querySelector(".container");
+cont.style.background = "red";
+cont.style.margin = "50px";
+cont.style.padding = "50px";
+cont.style.fontSize = "18px";
+cont.style.fontWeight = "Bold";
+cont.style.height = "200px";
+cont.style.width = "300px";
